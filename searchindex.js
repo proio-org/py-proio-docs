da99ea2795610fc0042f54c42023ee9032c1811c
Travis CI User <travis@example.org>
2017-12-20 22:08:52 +0000

    Autogenerated via decibelcooper/proio Travis CI build: 115
    Commit: ee9597fc646fac39b3f7b94facc3dbdd6dc658e4

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["proio"],titleterms:{indic:0,proio:0,tabl:0}})
